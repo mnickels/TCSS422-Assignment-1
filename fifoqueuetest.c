3f91ddb32711a736a88df4f8fbf2802bcc252fbd
mnickels <mc.nickels2@gmail.com>
2017-10-08 19:56:11 -0700

    removed extra } in fifoqueuetest

--- fifoqueuetest.c
+++ fifoqueuetest.c
@@ -1,5 +1,5 @@
 #include "pcb.h"
-#include "fifoqueue.h" 
+#include "fifoqueue.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,11 +7,11 @@
 int main(void) {
 	// Create a queue on the heap
     FIFO_q_p queue = fifo_q_new();
-	
+
 	//buffers for tostring functions
 	char fifostring[] = "";
 	char pcbstring[] = "";
-	
+
 	// Populate the queue with a random number of PCBs (between 10 and 30
 	int i;
 	int j = (int)((double)rand() / ((double)RAND_MAX + 1 - 10) * 30);
@@ -20,18 +20,16 @@ int main(void) {
 		PCB_p process = pcb_new();
 		pcb_set_priority(process, priority);
 		printf("%s", fifo_q_to_string(queue, fifostring));
-		
 	}
-	
+
 	printf(" : contents: ");
-	
+
 	Node_p tempnode = queue->front;
 	int i = 0;
 	while (tempnode) {
 	    printf("%s ", pcb_to_string(tempnode->process, pcbstring));
 	    tempnode = tempnode->next;
-		}
     }
-	
+
     fifo_q_destroy(queue);
 }
